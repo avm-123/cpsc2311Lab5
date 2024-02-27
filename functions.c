4547869a83557a868478ec7f4031e9abee8ff2c8
Aidan Militzer <115504926+avm-123@users.noreply.github.com>
2024-02-26 17:07:02 -0800

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,1 +1,111 @@
+/*************************
+*Aidan Militzer
+*CPSC 2310 Spring 24
+*amilitz
+*Instructor: Dr. Yvon Feaster
+*************************/
+#include "functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+ 
 
+
+// Function to create a linked list from data in a file
+node_t* createList(FILE* fp, node_t** head)
+{
+    node_t* newNode;
+    while (!feof(fp)) {
+// Read node information from the file
+        newNode = readNodeInfo(fp);
+// Add the newly created node to the list
+        add(&newNode, head);
+    }
+
+    return *head;
+}
+// Function to add a node to the end of the linked list
+void add(node_t** node,node_t** head)
+{
+// Check if the node is NULL
+     if (*node == NULL) {
+        return; 
+    }
+// Check if the list is empty
+    if (*head == NULL)
+    {
+// If the list is empty, set the head to the new node
+        *head = *node;
+//Set the next pointer of the new node to NULL
+        (*node)->next = NULL;
+    }
+    else
+    {
+// If the list is not empty, find the last node and append the new node
+        node_t* temp = *head;
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = *node;
+        (*node)->next = NULL;
+    }
+   
+}
+// Function to read node information from a file
+node_t* readNodeInfo(FILE* input)
+{
+// Allocate memory for the new node
+    node_t* newNode = (node_t*)malloc(sizeof(node_t));
+    if (newNode == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+// Read node data from the file
+    int result = fscanf(input, "%49[^,],%49[^,],%d,%d,%d,%9[^,],%14[^\n]", 
+                        newNode->data.lastName, newNode->data.firstName,
+                        &newNode->data.birthday.day, &newNode->data.birthday.month, &newNode->data.birthday.year,
+                        newNode->data.major, newNode->data.classStand);
+// Check if all data fields were successfully read
+    if (result != 7) { 
+        fprintf(stderr, "Error reading data from file\n");
+        free(newNode);
+        return NULL;
+    }
+    
+
+    return newNode;
+}
+// Function to print the linked list to a file
+void printList(FILE* output, node_t* head)
+{
+    while (head != NULL) {
+// Print a border before each node information
+        printBorder(output);
+ // Print node information to the output file
+        fprintf(output, "LIST INFO:\nName: %s %s\nDate of Birth: %d/%d/%d\nMajor: %s\nYear: %s\n",
+                head->data.firstName, head->data.lastName, head->data.birthday.month,
+                head->data.birthday.day, head->data.birthday.year, head->data.major,
+                head->data.classStand);
+// Move to the next node
+        head = head->next;
+        
+    }
+}
+// Function to delete the linked list and free memory
+void deleteList(node_t** head) {
+    node_t* current = *head;
+    node_t* next;
+// Loop through each node and free memory
+    while (current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+
+    *head = NULL;
+}
+
+// Function to print a border to the output file
+void printBorder(FILE* output) {
+    fprintf(output, "********************************************************************************\n");
+}
